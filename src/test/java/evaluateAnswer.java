9242def2bbdd281c348272dfdc9d6ac41512c388
Sergio <17knight2000@gmail.com>
2019-11-21 19:32:15 +0000

    commit 6 -bem feito

--- src/test/java/evaluateAnswer.java
+++ src/test/java/evaluateAnswer.java
@@ -1,4 +1,5 @@
 import implementation.controller.Test;
+import implementation.models.QuestionMultipleChoice;
 import implementation.models.QuestionNumeric;
 import implementation.models.QuestionYesNo;
 import interfaces.controller.ITest;
@@ -17,7 +18,7 @@ public class evaluateAnswer {
      * Test result: FAILDED
      **/
     @org.junit.jupiter.api.Test
-    public void TestingWithUserAwnserNull() {
+    public void TestingWithUserAnswerNull() {
         ITest test = new Test();
         QuestionYesNo question3 = new QuestionYesNo("Questao yes or no?", "1 + 3 = 4 ?");
         question3.setCorrect_answer("Yes");
@@ -30,4 +31,74 @@ public class evaluateAnswer {
 
 
     }
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: true
+     * Output: true
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswerCorrectYesOrNo() {
+        ITest test = new Test();
+        QuestionYesNo question3 = new QuestionYesNo("Questao yes or no?", "1 + 3 = 4 ?");
+        question3.setCorrect_answer("Yes");
+
+
+        question3.setUser_answer("Yes");
+
+
+        Assertions.assertTrue(question3.evaluateAnswer());
+
+
+
+
+
+    }
+
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: true
+     * Output: true
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswerCorrectNumeric() {
+        ITest test = new Test();
+        QuestionNumeric question1 = new QuestionNumeric(2.0, "Questão-1", "Quanto é 1+1?");
+
+
+
+        question1.setUser_answer(2.0);
+
+
+        Assertions.assertTrue(question1.evaluateAnswer());
+    }
+
+
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: true
+     * Output: true
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswserCorrect() {
+        ITest test = new Test();
+        String[] options = {"4","5","6","7"};
+        QuestionMultipleChoice question2 = new QuestionMultipleChoice("Escolha a opcao correta", "Quanto é 3+3?",options,"6");
+
+
+
+        question2.setUser_answer("6");
+
+
+        Assertions.assertTrue(question2.evaluateAnswer());
+    }
+
+
 }

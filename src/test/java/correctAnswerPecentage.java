7ac5e6a58b2be18dc1a933c2d55450ec5b382460
Sergio <17knight2000@gmail.com>
2019-11-21 18:14:20 +0000

    commit 3 -bem feito

--- src/test/java/correctAnswerPecentage.java
+++ src/test/java/correctAnswerPecentage.java
@@ -13,12 +13,12 @@ public class correctAnswerPecentage {
     /**
      * ID:  - válido
      * Input: -
-     * Expected Output: 0.5
+     * Expected Output: 1
      * Output: 0
-     * Test result: FAILED
+     * Test result: PASSED
      **/
     @org.junit.jupiter.api.Test
-    public void TestingWithAllHalfofTheQuestionCorret(){
+    public void TestingWithAllQuestionCorrect(){
         ITest test = new Test();
         QuestionNumeric question1 = new QuestionNumeric(2.0, "Questão-1", "Quanto é 1+1?");
         QuestionNumeric question2 = new QuestionNumeric(4.0, "Questão-2", "Quanto é 2+2?");
@@ -32,19 +32,89 @@ public class correctAnswerPecentage {
             question1.setUser_answer(2.0); //certa
             question2.setUser_answer(4.0); //certa
             question3.setUser_answer(6.0); // certa
-            question4.setUser_answer(8.0); //errada
+            question4.setUser_answer(8.0); //certa
             question1.setDone(true);
             question2.setDone(true);
             question3.setDone(true);
             question4.setDone(true);
             System.out.println(test.getTestStatistics().correctAnswer());
 
-            Assertions.assertEquals(0.75,test.getTestStatistics().correctAnswerPecentage());
+            Assertions.assertEquals(1.0,test.getTestStatistics().correctAnswerPecentage());
         } catch (TestException e) {
         }
     }
 
 
-   
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: 0
+     * Output: 0
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithAllTheQuestionInCorrect(){
+        ITest test = new Test();
+        QuestionNumeric question1 = new QuestionNumeric(2.0, "Questão-1", "Quanto é 1+1?");
+        QuestionNumeric question2 = new QuestionNumeric(4.0, "Questão-2", "Quanto é 2+2?");
+        QuestionNumeric question3 = new QuestionNumeric(6.0, "Questão-3", "Quanto é 3+3?");
+        QuestionNumeric question4 = new QuestionNumeric(8.0, "Questão-3", "Quanto é 4+4?");
+        try {
+            test.addQuestion(question1);
+            test.addQuestion(question2);
+            test.addQuestion(question3);
+            test.addQuestion(question4);
+            question1.setUser_answer(1.0); //errada
+            question2.setUser_answer(2.0); //errada
+            question3.setUser_answer(4.0); // errada
+            question4.setUser_answer(9.0); //errada
+            question1.setDone(true);
+            question2.setDone(true);
+            question3.setDone(true);
+            question4.setDone(true);
+            System.out.println(test.getTestStatistics().correctAnswer());
+
+            Assertions.assertEquals(0,test.getTestStatistics().correctAnswerPecentage());
+        } catch (TestException e) {
+        }
+    }
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: 0.5
+     * Output: 0.5
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithAllHalfofTheQuestionCorrect(){
+        ITest test = new Test();
+        QuestionNumeric question1 = new QuestionNumeric(2.0, "Questão-1", "Quanto é 1+1?");
+        QuestionNumeric question2 = new QuestionNumeric(4.0, "Questão-2", "Quanto é 2+2?");
+        QuestionNumeric question3 = new QuestionNumeric(6.0, "Questão-3", "Quanto é 3+3?");
+        QuestionNumeric question4 = new QuestionNumeric(8.0, "Questão-3", "Quanto é 4+4?");
+        try {
+            test.addQuestion(question1);
+            test.addQuestion(question2);
+            test.addQuestion(question3);
+            test.addQuestion(question4);
+            question1.setUser_answer(1.0); //errada
+            question2.setUser_answer(4.0); //certa
+            question3.setUser_answer(4.0); // errada
+            question4.setUser_answer(8.0); //certa
+            question1.setDone(true);
+            question2.setDone(true);
+            question3.setDone(true);
+            question4.setDone(true);
+            System.out.println(test.getTestStatistics().correctAnswer());
+
+            Assertions.assertEquals(0.5,test.getTestStatistics().correctAnswerPecentage());
+        } catch (TestException e) {
+        }
+    }
+
+
+
+
 
 }

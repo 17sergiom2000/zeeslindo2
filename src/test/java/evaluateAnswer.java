b76a40e79940286077f59a39f5a0a38decaeeb8b
Sergio <17knight2000@gmail.com>
2019-11-21 19:37:58 +0000

    commit 6 -bem feito

--- src/test/java/evaluateAnswer.java
+++ src/test/java/evaluateAnswer.java
@@ -22,13 +22,8 @@ public class evaluateAnswer {
         ITest test = new Test();
         QuestionYesNo question3 = new QuestionYesNo("Questao yes or no?", "1 + 3 = 4 ?");
         question3.setCorrect_answer("Yes");
-
-
-
+        
           Assertions.assertThrows(QuestionException.class, question3::evaluateAnswer);
-
-
-
 
     }
     /**
@@ -87,7 +82,7 @@ public class evaluateAnswer {
      * Test result: PASSED
      **/
     @org.junit.jupiter.api.Test
-    public void TestingWithUserAnswserCorrect() {
+    public void TestingWithUserAnswerCorrect() {
         ITest test = new Test();
         String[] options = {"4","5","6","7"};
         QuestionMultipleChoice question2 = new QuestionMultipleChoice("Escolha a opcao correta", "Quanto é 3+3?",options,"6");
@@ -100,5 +95,74 @@ public class evaluateAnswer {
         Assertions.assertTrue(question2.evaluateAnswer());
     }
 
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: false
+     * Output: false
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswerInCorrectYesOrNo() {
+        ITest test = new Test();
+        QuestionYesNo question3 = new QuestionYesNo("Questao yes or no?", "1 + 3 = 4 ?");
+        question3.setCorrect_answer("Yes");
+
+
+        question3.setUser_answer("No");
+
+
+        Assertions.assertFalse(question3.evaluateAnswer());
+
+
+
+
+
+    }
+
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: false
+     * Output: false
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswerInCorrectNumeric() {
+        ITest test = new Test();
+        QuestionNumeric question1 = new QuestionNumeric(2.0, "Questão-1", "Quanto é 1+1?");
+
+
+
+        question1.setUser_answer(3.0);
+
+
+        Assertions.assertFalse(question1.evaluateAnswer());
+    }
+
+
+
+    /**
+     * ID:  - válido
+     * Input: -
+     * Expected Output: false
+     * Output: false
+     * Test result: PASSED
+     **/
+    @org.junit.jupiter.api.Test
+    public void TestingWithUserAnswerInCorrect() {
+        ITest test = new Test();
+        String[] options = {"4","5","6","7"};
+        QuestionMultipleChoice question2 = new QuestionMultipleChoice("Escolha a opcao correta", "Quanto é 3+3?",options,"6");
+
+
+
+        question2.setUser_answer("7");
+
+
+        Assertions.assertFalse(question2.evaluateAnswer());
+    }
 
 }
